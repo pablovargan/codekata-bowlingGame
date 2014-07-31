c44f7aca4cebda8795cfae37a3f26309595d9d52
Pablo Varela <pablovargan@gmail.com>
2014-07-31 10:50:50 +0200

    First test created

---
+++ BowlingGameTest/Test1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using BowlingGame;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BowlingGameTest
+{
+    [TestClass]
+    public class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Game g = new Game();
+        }
+
+    }
+}
